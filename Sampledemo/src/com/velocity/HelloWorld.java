16e6df17f4d334cf4f0b8138b4e61b30be0c3c4e
Swapnil91 <swapnilshitole606@gmail.com>
2023-08-30 18:06:26 +0530

    Added hello world

---
+++ Sampledemo/src/com/velocity/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.velocity;
+
+public class HelloWorld {
+	public void hello() {
+		System.out.println("Hello world ");
+	}
+	public static void main(String[] args) {
+		HelloWorld helloWorld= new HelloWorld();
+		helloWorld.hello();
+	}
+
+}
