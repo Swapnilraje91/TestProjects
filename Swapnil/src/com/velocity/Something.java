22c732e4dc7dacaf256a604d808063173d91e5f7
Swapnil91 <swapnilshitole606@gmail.com>
2023-09-01 15:59:00 +0530

    Arraylist added

---
+++ Swapnil/src/com/velocity/Something.java
@@ -0,0 +1,18 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Something {
+	public static void main(String[] args) {
+		ArrayList arrayList= new ArrayList();
+		arrayList.add(10);
+		arrayList.add("Swapnil");
+		arrayList.add("Shitole");
+		arrayList.add(52);
+		arrayList.add("Prathmesh");
+		arrayList.add("Apurva");
+		
+		System.out.println(arrayList);
+		
+	}
+}
